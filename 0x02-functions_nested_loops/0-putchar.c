b0834759c68dcea39c2912b623072369e4f046e6
Daniel Yohannes <dyohannes764@gmail.com>
2023-03-21 10:23:50 -0700

    Nested loops and functions

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - entery point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	printf("__putchar");
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,2 @@
+#!/bin/bash
+Nasted loops and functions
