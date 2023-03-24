34c4c8ee131c9a732784451b8503999124df0748
Daniel Yohannes <dyohannes764@gmail.com>
2023-03-24 09:49:53 -0700

    More Function, More Nasted loops

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+utable File  12 lines (11 sloc)  174 Bytes
+
+#include "main.h"
+
+/**
+ * mul - multiplies two integers.
+ * @a: first number.
+ * @b: second number.
+ * Return: multiplication.
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
