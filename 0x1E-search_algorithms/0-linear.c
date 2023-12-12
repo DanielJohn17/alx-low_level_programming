50872df0b16fd9a1b6afcea3663b65368f513dfc
DanielJohn17 <danielyohannes38@gmail.com>
2023-12-12 15:26:02 +0300

    fix

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -6,7 +6,7 @@
  * @size: size of the array
  * @value: number to be searched in the array
  *
- * Return: index of the value or -1 in failure
+ * Return: Always EXIT_SUCCESS
 */
 int linear_search(int *array, size_t size, int value)
 {
